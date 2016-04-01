e2796e926e61952a44211926b56325b7b6095ca4
Victor Powell <vp@uber.com>
2016-03-31 17:38:26 -0700

    Fix tests on Travis-CI by using testron.

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var document = require('global/document');
-var test = require('prova');
 var MapGL = require('../src/index');
+var test = require('tape');
 var React = require('react');
 var r = require('r-dom');
 var process = require('global/process');
@@ -12,20 +12,18 @@ var mapboxApiAccessToken = process.env.MapboxAccessToken;
 /* eslint-enable no-process-env */
 
 /* eslint-disable func-names, no-shadow */
-test('MapGL', function(t) {
-  t.test('Exists', function(t) {
-    t.ok(MapGL);
-    var map = r(MapGL, {
-      width: 500,
-      height: 500,
-      longitude: -122,
-      latitude: 37,
-      zoom: 14,
-      mapboxApiAccessToken: mapboxApiAccessToken
-    });
-    React.render(map, document.body);
-    t.ok(true);
-    t.end();
+test('Exists', function(t) {
+  t.ok(MapGL);
+  var map = r(MapGL, {
+    width: 500,
+    height: 500,
+    longitude: -122,
+    latitude: 37,
+    zoom: 14,
+    mapboxApiAccessToken: mapboxApiAccessToken
   });
+  React.render(map, document.body);
+  t.ok(true);
+  t.end();
 });
 /* eslint-enable func-names */
