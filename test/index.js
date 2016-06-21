fdd5fdb7b7438f7bc7d944ad2bb148d50c79d712
Ashley Travers <atravers@uber.com>
2016-06-20 21:52:25 -0700

    update tests

--- test/index.js
+++ test/index.js
@@ -5,7 +5,6 @@ var test = require('tape');
 var MapGL = require('../');
 var React = require('react');
 var ReactDOM = require('react-dom');
-var r = require('r-dom');
 var process = require('global/process');
 
 /* eslint-disable no-process-env */
@@ -17,14 +16,15 @@ var mapboxApiAccessToken = process.env.MapboxAccessToken;
 test('MapGL', function(t) {
   t.test('Exists', function(t) {
     t.ok(MapGL);
-    var map = r(MapGL, {
-      width: 500,
-      height: 500,
-      longitude: -122,
-      latitude: 37,
-      zoom: 14,
-      mapboxApiAccessToken: mapboxApiAccessToken
-    });
+    const map = (
+      <MapGL
+        width={ 500 }
+        height={ 500 }
+        longitude={ -122 }
+        latitude={ 37 }
+        zoom={ 14 }
+        mapboxApiAccessToken={ mapboxApiAccessToken }/>
+    );
     var reactContainer = document.createElement('div');
     document.body.appendChild(reactContainer);
     ReactDOM.render(map, reactContainer);
