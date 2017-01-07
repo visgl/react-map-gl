ecdc2bf1155c369173ff579fd044263316694532
Balthazar Gronon <balthazar@uber.com>
2017-01-06 18:46:14 -0800

    Add onLoad event handler

--- test/map-spec.js
+++ test/map-spec.js
@@ -1,34 +1,44 @@
 import MapGL from '../src';
 
-import document from 'global/document';
-/* global process */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import document from 'global/document';
 import test from 'tape-catch';
 
-/* eslint-disable no-process-env */
-// This will get converted to a string by envify
-const mapboxApiAccessToken = process.env.MapboxAccessToken;
-/* eslint-enable no-process-env */
+const mapboxApiAccessToken = process.env.MapboxAccessToken || process.env.MAPBOX_ACCESS_TOKEN; // eslint-disable-line
 
-/* eslint-disable func-names, no-shadow */
-test('MapGL', function(t) {
-  t.test('Exists', function(t) {
-    t.ok(MapGL);
-    const map = (
-      <MapGL
-        width={ 500 }
-        height={ 500 }
-        longitude={ -122 }
-        latitude={ 37 }
-        zoom={ 14 }
-        mapboxApiAccessToken={ mapboxApiAccessToken }/>
-    );
-    const reactContainer = document.createElement('div');
-    document.body.appendChild(reactContainer);
-    ReactDOM.render(map, reactContainer);
-    t.ok(true);
-    t.end();
-  });
+const defaultProps = {
+  width: 500,
+  height: 500,
+  longitude: -122,
+  latitude: 37,
+  zoom: 14,
+  mapboxApiAccessToken
+};
+
+test('MapGL can mount', t => {
+  t.ok(MapGL);
+  const reactContainer = document.createElement('div');
+  document.body.appendChild(reactContainer);
+  ReactDOM.render(<MapGL {...defaultProps} />, reactContainer);
+  t.ok(true);
+  t.end();
 });
-/* eslint-enable func-names */
+
+test('MapGL call onLoad when provided', t => {
+  const reactContainer = document.createElement('div');
+  document.body.appendChild(reactContainer);
+
+  function onLoad(...args) {
+    t.equal(args.length, 0, 'onLoad does not expose the map object.');
+    t.end();
+  }
+
+  const props = {...defaultProps, onLoad};
+  ReactDOM.render(<MapGL {...props} />, reactContainer);
+
+  if (!MapGL.supported()) {
+    t.end();
+  }
+
+});
