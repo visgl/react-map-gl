5fbd6cfbc47895d99349e30c6bea419e704c7fdd
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2017-05-02 17:39:14 -0700

    Separating input events and semantic transform in map control (#212)

--- src/components/map-controls.js
+++ src/components/map-controls.js
@@ -17,53 +17,23 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-import {PerspectiveMercatorViewport} from 'viewport-mercator-project';
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
-import assert from 'assert';
 
 import autobind from '../utils/autobind';
 
 // MapControls uses non-react event manager to register events
 import EventManager from '../utils/event-manager';
+import MapState from '../utils/map-state';
 
 import config from '../config';
-
-function mod(value, divisor) {
-  const modulus = value % divisor;
-  return modulus < 0 ? divisor + modulus : modulus;
-}
-// MAPBOX LIMITS
-const MAX_PITCH = 60;
-const MAX_ZOOM = 40;
 
 // EVENT HANDLING PARAMETERS
 const PITCH_MOUSE_THRESHOLD = 5;
 const PITCH_ACCEL = 1.2;
+const ZOOM_ACCEL = 0.01;
 
-const propTypes = {
-  /** The width of the map */
-  width: PropTypes.number.isRequired,
-  /** The height of the map */
-  height: PropTypes.number.isRequired,
-  /** The latitude of the center of the map. */
-  latitude: PropTypes.number.isRequired,
-  /** The longitude of the center of the map. */
-  longitude: PropTypes.number.isRequired,
-  /** The tile zoom level of the map. */
-  zoom: PropTypes.number.isRequired,
-  /** Specify the bearing of the viewport */
-  bearing: React.PropTypes.number,
-  /** Specify the pitch of the viewport */
-  pitch: React.PropTypes.number,
-  /**
-    * Specify the altitude of the viewport camera
-    * Unit: map heights, default 1.5
-    * Non-public API, see https://github.com/mapbox/mapbox-gl-js/issues/1137
-    */
-  altitude: React.PropTypes.number,
-
-  constraints: React.PropTypes.object,
+const propTypes = Object.assign({}, MapState.propTypes, {
 
   /** Enables perspective control event handling */
   perspectiveEnabled: PropTypes.bool,
@@ -79,37 +49,19 @@ const propTypes = {
     * drag cursor. Also used as an optimization in some overlays by preventing
     * rendering while dragging.
     */
+  isHovering: PropTypes.bool,
   isDragging: PropTypes.bool,
-  /**
-    * Required to calculate the mouse projection after the first click event
-    * during dragging. Where the map is depends on where you first clicked on
-    * the map.
-    */
-  startDragLngLat: PropTypes.arrayOf(PropTypes.number),
-  /** Bearing when current perspective drag operation started */
-  startBearing: PropTypes.number,
-  /** Pitch when current perspective drag operation started */
-  startPitch: PropTypes.number,
 
   /**
     * True means key must be pressed to rotate instead of pan
     * false to means key must be pressed to pan
     */
   pressKeyToRotate: PropTypes.bool
-};
+});
 
 const defaultProps = {
-  bearing: 0,
-  pitch: 0,
-  altitude: 1.5,
-  clickRadius: 15,
+  perspectiveEnabled: false,
   onChangeViewport: null,
-
-  maxZoom: MAX_ZOOM,
-  minZoom: 0,
-  maxPitch: MAX_PITCH,
-  minPitch: 0,
-
   pressKeyToRotate: true
 };
 
@@ -122,13 +74,6 @@ export default class MapControls extends PureComponent {
    */
   constructor(props) {
     super(props);
-    this.state = {
-      isDragging: false,
-      isHovering: false,
-      startDragLngLat: null,
-      startBearing: null,
-      startPitch: null
-    };
     autobind(this);
   }
 
@@ -142,27 +87,12 @@ export default class MapControls extends PureComponent {
 
     this._eventManager = new EventManager(this.refs.canvas, {
       onMouseDown: this._onMouseDown,
-      // onMouseMove: is bound only after a mouse down is detected
       onMouseDrag: this._onMouseDrag,
-      onMouseRotate: this._onMouseRotate,
       onMouseUp: this._onMouseUp,
-      onTouchStart: this._onTouchStart,
-      onTouchDrag: this._onTouchDrag,
       onTouchRotate: this._onTouchRotate,
-      onTouchEnd: this._onTouchEnd,
-      onTouchTap: this._onTouchTap,
-      onZoom: this._onZoom,
-      onZoomEnd: this._onZoomEnd,
-      mapTouchToMouse: true,
-      pressKeyToRotate: this.props.pressKeyToRotate
-    });
-  }
-
-  // New props are comin' round the corner!
-  componentWillReceiveProps(newProps) {
-    const {startDragLngLat} = newProps;
-    this.setState({
-      startDragLngLat: startDragLngLat && [...startDragLngLat]
+      onWheel: this._onWheel,
+      onWheelEnd: this._onWheelEnd,
+      mapTouchToMouse: true
     });
   }
 
@@ -179,158 +109,41 @@ export default class MapControls extends PureComponent {
     if (this.props.isDragging) {
       return config.CURSOR.GRABBING;
     }
-    if (this.state.isHovering) {
+    if (this.props.isHovering) {
       return config.CURSOR.POINTER;
     }
     return config.CURSOR.GRAB;
   }
 
-  _updateViewport(opts) {
-    let viewport = Object.assign({
-      latitude: this.props.latitude,
-      longitude: this.props.longitude,
-      zoom: this.props.zoom,
-      bearing: this.props.bearing,
-      pitch: this.props.pitch,
-      altitude: this.props.altitude,
-      isDragging: this.props.isDragging,
-      startDragLngLat: this.props.startDragLngLat,
-      startBearing: this.props.startBearing,
-      startPitch: this.props.startPitch
-    }, opts);
-
-    viewport = this._applyConstraints(viewport);
-
-    // if (viewport.startDragLngLat) {
-    //   const dragViewport = new FlatMercatorViewport(Object.assign({}, this.props, {
-    //     longitude: viewport.startDragLngLat[0],
-    //     latitude: viewport.startDragLngLat[1]
-    //   }));
-    //   this.setState({dragViewport});
-    // }
-
-    return this.props.onChangeViewport(viewport);
-  }
-
-  // Apply any constraints (mathematical or defined by props) to viewport params
-  _applyConstraints(viewport) {
-    // Normalize degrees
-    viewport.longitude = mod(viewport.longitude + 180, 360) - 180;
-    viewport.bearing = mod(viewport.bearing + 180, 360) - 180;
-
-    // Ensure zoom is within specified range
-    const {maxZoom, minZoom} = this.props;
-    viewport.zoom = viewport.zoom > maxZoom ? maxZoom : viewport.zoom;
-    viewport.zoom = viewport.zoom < minZoom ? minZoom : viewport.zoom;
-
-    // Ensure pitch is within specified range
-    const {maxPitch, minPitch} = this.props;
-
-    viewport.pitch = viewport.pitch > maxPitch ? maxPitch : viewport.pitch;
-    viewport.pitch = viewport.pitch < minPitch ? minPitch : viewport.pitch;
-
-    return viewport;
-  }
-
-  _unproject(pos) {
-    const viewport = new PerspectiveMercatorViewport(this.props);
-    return viewport.unproject(pos, {topLeft: false});
-  }
-
-  // Calculate a new lnglat based on pixel dragging position
-  // TODO - We should have a mapbox-independent implementation of panning
-  // Panning calculation is currently done using an undocumented mapbox function
-  _calculateNewLngLat({startDragLngLat, pos, startPos}) {
-    const viewport = new PerspectiveMercatorViewport(this.props);
-
-    return viewport.getLocationAtPoint({lngLat: startDragLngLat, pos});
-  }
-
-  // Calculates new zoom
-  _calculateNewZoom({relativeScale}) {
-    return this.props.zoom + Math.log2(relativeScale);
-  }
-
-  // Calculates a new pitch and bearing from a position (coming from an event)
-  _calculateNewPitchAndBearing({pos, startPos, startBearing, startPitch}) {
-    const {maxPitch} = this.props;
-    // TODO minPitch
-
-    const xDelta = pos[0] - startPos[0];
-    const yDelta = pos[1] - startPos[1];
-
-    const bearing = startBearing + 180 * xDelta / this.props.width;
-
-    let pitch = startPitch;
-    if (yDelta > 0) {
-      // Dragging downwards, gradually decrease pitch
-      if (Math.abs(this.props.height - startPos[1]) > PITCH_MOUSE_THRESHOLD) {
-        const scale = yDelta / (this.props.height - startPos[1]);
-        pitch = (1 - scale * PITCH_ACCEL) * startPitch;
-      }
-    } else if (yDelta < 0) {
-      // Dragging upwards, gradually increase pitch
-      if (startPos[1] > PITCH_MOUSE_THRESHOLD) {
-        // Move from 0 to 1 as we drag upwards
-        const yScale = 1 - pos[1] / startPos[1];
-        // Gradually add until we hit max pitch
-        pitch = startPitch + yScale * (maxPitch - startPitch);
-      }
-    }
-
-    return {
-      pitch,
-      bearing
-    };
-  }
-
-  _onTouchStart(opts) {
-    this._onMouseDown(opts);
-  }
-
-  _onTouchDrag(opts) {
-    this._onMouseDrag(opts);
+  _updateViewport(...opts) {
+    const {isDragging} = this.props;
+    return this.props.onChangeViewport(Object.assign({isDragging}, ...opts));
   }
 
   _onTouchRotate(opts) {
     this._onMouseRotate(opts);
   }
 
-  _onTouchEnd(opts) {
-    this._onMouseUp(opts);
-  }
-
-  _onTouchTap(opts) {
-    this._onMouseClick(opts);
-  }
-
   _onMouseDown({pos}) {
-    this._updateViewport({
-      isDragging: true,
-      startDragLngLat: this._unproject(pos),
-      startBearing: this.props.bearing,
-      startPitch: this.props.pitch
-    });
+    const mapState = new MapState(this.props).panStart({pos}).rotateStart({pos});
+    this._updateViewport(mapState.props, {isDragging: true});
   }
 
-  _onMouseDrag({pos}) {
+  _onMouseDrag({pos, startPos, modifier}) {
     if (!this.props.onChangeViewport) {
       return;
     }
 
-    const {startDragLngLat} = this.state;
+    if (this.props.pressKeyToRotate === modifier) {
+      this._onMouseRotate({pos, startPos});
+    } else {
+      this._onMousePan({pos});
+    }
+  }
 
-    // take the start lnglat and put it where the mouse is down.
-    assert(startDragLngLat, '`startDragLngLat` prop is required ' +
-      'for mouse drag behavior to calculate where to position the map.');
-
-    const [longitude, latitude] = this._calculateNewLngLat({startDragLngLat, pos});
-
-    this._updateViewport({
-      longitude,
-      latitude,
-      isDragging: true
-    });
+  _onMousePan({pos}) {
+    const mapState = new MapState(this.props).pan({pos});
+    this._updateViewport(mapState.props);
   }
 
   _onMouseRotate({pos, startPos}) {
@@ -338,54 +151,46 @@ export default class MapControls extends PureComponent {
       return;
     }
 
-    const {startBearing, startPitch} = this.props;
-    assert(typeof startBearing === 'number',
-      '`startBearing` prop is required for mouse rotate behavior');
-    assert(typeof startPitch === 'number',
-      '`startPitch` prop is required for mouse rotate behavior');
+    const xDelta = pos[0] - startPos[0];
+    const yDelta = pos[1] - startPos[1];
 
-    const {pitch, bearing} = this._calculateNewPitchAndBearing({
-      pos,
-      startPos,
-      startBearing,
-      startPitch
-    });
+    const xDeltaScale = xDelta / this.props.width;
+    let yDeltaScale = 0;
 
-    this._updateViewport({
-      bearing,
-      pitch,
-      isDragging: true
-    });
+    if (yDelta > 0) {
+      if (Math.abs(this.props.height - startPos[1]) > PITCH_MOUSE_THRESHOLD) {
+        // Move from 0 to -1 as we drag upwards
+        yDeltaScale = yDelta / (startPos[1] - this.props.height) * PITCH_ACCEL;
+      }
+    } else if (yDelta < 0) {
+      if (startPos[1] > PITCH_MOUSE_THRESHOLD) {
+        // Move from 0 to 1 as we drag upwards
+        yDeltaScale = 1 - pos[1] / startPos[1];
+      }
+    }
+
+    const mapState = new MapState(this.props).rotate({xDeltaScale, yDeltaScale});
+    this._updateViewport(mapState.props);
   }
 
-  _onMouseUp(opt) {
-    this._updateViewport({
-      isDragging: false,
-      startDragLngLat: null,
-      startBearing: null,
-      startPitch: null
-    });
+  _onMouseUp() {
+    const mapState = new MapState(this.props).panEnd().rotateEnd();
+    this._updateViewport(mapState.props, {isDragging: false});
   }
 
-  _onZoom({pos, scale}) {
-    // Make sure we zoom around the current mouse position rather than map center
-    const aroundLngLat = this._unproject(pos);
+  _onWheel({pos, delta}) {
+    let scale = 2 / (1 + Math.exp(-Math.abs(delta * ZOOM_ACCEL)));
+    if (delta < 0 && scale !== 0) {
+      scale = 1 / scale;
+    }
 
-    const zoom = this._calculateNewZoom({relativeScale: scale});
-
-    const zoomedViewport = new PerspectiveMercatorViewport(Object.assign({}, this.props, {zoom}));
-    const [longitude, latitude] = zoomedViewport.getLocationAtPoint({lngLat: aroundLngLat, pos});
-
-    this._updateViewport({
-      zoom,
-      longitude,
-      latitude,
-      isDragging: true
-    });
+    const mapState = new MapState(this.props).zoom({pos, startPos: pos, scale});
+    this._updateViewport(mapState.props, {isDragging: true});
   }
 
-  _onZoomEnd() {
-    this._updateViewport({isDragging: false});
+  _onWheelEnd() {
+    const mapState = new MapState(this.props).zoomEnd();
+    this._updateViewport(mapState.props, {isDragging: false});
   }
 
   render() {
