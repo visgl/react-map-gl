661608ee092f4eeecb3b89aa6f5de840e70956a0
Xiaoji Chen <xiaoji@uber.com>
2017-05-03 11:51:56 -0700

    typo

--- src/components/map-controls.js
+++ src/components/map-controls.js
@@ -118,7 +118,7 @@ export default class MapControls extends PureComponent {
     return this.props.onChangeViewport(Object.assign(
       {isDragging},
       mapState.getViewportProps(),
-      ...extraState
+      extraState
     ));
   }
 
