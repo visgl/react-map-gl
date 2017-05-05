da23dde556cb5b967facec166762d605d0dfe257
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2017-05-05 10:50:47 -0700

    Popup control (#227)

--- examples/controls/components/city-pin.js
+++ examples/controls/components/city-pin.js
@@ -7,11 +7,12 @@ const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,
 export default class CityPin extends PureComponent {
 
   render() {
-    const {size = 20} = this.props;
+    const {size = 20, onClick} = this.props;
 
     return (
       <svg height={size} viewBox='0 0 24 24' className="city-pin"
-        style={{transform: `translate(${-size/2}px,${-size}px)`}} >
+        style={{transform: `translate(${-size/2}px,${-size}px)`}}
+        onClick={onClick} >
         <path d={ICON}/>
       </svg>
     );
--- test/utils/index.js
+++ test/utils/index.js
@@ -1,3 +1,4 @@
 // import './fit-bounds.spec';
 import './style-utils.spec';
 import './map-state.spec';
+import './dynamic-position.spec';
