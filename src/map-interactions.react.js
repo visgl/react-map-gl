31d5ec1a1d09a6512d1579adfb0452142c8a9b50
Anh Mai <amai@uber.com>
2017-05-18 11:59:29 -0700

    add prop to disable scroll zoom

--- src/map-interactions.react.js
+++ src/map-interactions.react.js
@@ -93,7 +93,8 @@ export default class Interactions extends Component {
     onTouchEnd: PropTypes.func,
     onTouchTap: PropTypes.func,
     onZoom: PropTypes.func,
-    onZoomEnd: PropTypes.func
+    onZoomEnd: PropTypes.func,
+    scrollZoomEnabled: PropTypes.bool
   };
 
   static defaultProps = {
@@ -109,7 +110,8 @@ export default class Interactions extends Component {
     onTouchEnd: noop,
     onTouchTap: noop,
     onZoom: noop,
-    onZoomEnd: noop
+    onZoomEnd: noop,
+    scrollZoomEnabled: true
   };
 
   constructor(props) {
@@ -225,6 +227,10 @@ export default class Interactions extends Component {
   /* eslint-disable complexity, max-statements */
   @autobind
   _onWheel(event) {
+    // Exit if scroll zoom isn't enabled
+    if (!this.props.scrollZoomEnabled) {
+      return;
+    }
     event.preventDefault();
     let value = event.deltaY;
     // Firefox doubles the values on retina screens...
