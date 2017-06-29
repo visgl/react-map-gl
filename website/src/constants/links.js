bd02730a07c576478f27cda9dc208c4b77aa632d
Xiaoji Chen <xiaoji@uber.com>
2017-06-29 16:17:06 -0700

    add links to other libraries

---
+++ website/src/constants/links.js
@@ -0,0 +1,7 @@
+export const LIBRARIES = {
+  'react-map-gl': '#',
+  // 'react-map-gl': 'https://uber.github.io/react-map-gl',
+  'deck.gl': 'https://uber.github.io/deck.gl',
+  'luma.gl': 'https://uber.github.io/luma.gl',
+  'react-vis': 'https://uber.github.io/react-vis'
+};
