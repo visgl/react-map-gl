7664add7e9fc39248e15f3b9dac92eaef82e242e
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2017-07-28 13:57:41 -0700

    Add filter example (#334)

---
+++ examples/filter/src/control-panel.js
@@ -0,0 +1,21 @@
+import React, {PureComponent} from 'react';
+
+const defaultContainer =  ({children}) => <div className="control-panel">{children}</div>;
+
+export default class ControlPanel extends PureComponent {
+
+  render() {
+    const Container = this.props.containerComponent || defaultContainer;
+    const {settings} = this.props;
+
+    return (
+      <Container>
+        <h3>Highlight Features Containing Similar Data</h3>
+        <p>Hover over counties to highlight counties that share the same name.</p>
+        <div className="source-link">
+          <a href="https://github.com/uber/react-map-gl/tree/master/examples/filter" target="_new">View Code ↗</a>
+        </div>
+      </Container>
+    );
+  }
+}
