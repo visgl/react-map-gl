87493f94ef42fd649028a7a1f1eecb3cdebbdaf1
Ib Green <ibgreen@users.noreply.github.com>
2017-09-18 19:32:50 -0700

    Add yarn linting (#360)

--- examples/geojson-animation/src/control-panel.js
+++ examples/geojson-animation/src/control-panel.js
@@ -11,7 +11,10 @@ export default class ControlPanel extends PureComponent {
         <h3>Animated GeoJSON</h3>
         <p>Render animation by updating GeoJSON data source.</p>
         <div className="source-link">
-          <a href="https://github.com/uber/react-map-gl/tree/master/examples/geojson-animation" target="_new">View Code ↗</a>
+          <a href="https://github.com/uber/react-map-gl/tree/master/examples/geojson-animation"
+            target="_new">
+            View Code ↗
+          </a>
         </div>
       </Container>
     );
