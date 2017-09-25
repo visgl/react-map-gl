0f5f802b825993883c862733ef42280b6c9de0f8
Ib Green <ib@uber.com>
2017-09-24 17:02:18 -0700

    Add examples to ocular

--- ocular/src/mdRoutes.js
+++ ocular/src/mdRoutes.js
@@ -16,21 +16,109 @@ import svgOverlay from '../../docs/overlays/svg-overlay.md';
 // import cumstomMapControls from 'docs/advanced/custom-map-controls.md';
 /* eslint-enable max-len */
 
+
+// Modules
+import MarkerExample from '../../examples/main/views/marker';
+import NotInteractiveExample from '../../examples/main/views/not-interactive';
+import CustomOverlayExample from '../../examples/main/views/custom-overlay';
+import RouteOverlayExample from '../../examples/main/views/route-overlay';
+import StyleDiffingExample from '../../examples/main/views/style-diffing';
+import ClickExample from '../../examples/main/views/click';
+// Standalone
+import Filter from '../../examples/filter/src/app';
+import Controls from '../../examples/controls/src/app';
+import GeoJson from '../../examples/geojson/src/app';
+import GeoJsonAnimation from '../../examples/geojson-animation/src/app';
+import Interaction from '../../examples/interaction/src/app';
+import Layers from '../../examples/layers/src/app';
+import ViewportAnimation from '../../examples/viewport-animation/src/app';
+
 export default [
-  // {
-  //   name: 'Examples',
-  //   path: '/examples',
-  //   data: [
-  //     // {
-  //     //   name: 'Overview',
-  //     //   markdown: overview
-  //     // },
-  //     {
-  //       name: 'Showcases',
-  //       children: []
-  //     }
-  //   ]
-  // },
+  {
+    name: 'Examples',
+    path: '/examples',
+    data: [
+      // {
+      //   name: 'Overview',
+      //   markdown: overview
+      // },
+      {
+        name: 'Showcases',
+        children: [
+          {
+            path: 'layersExample',
+            name: 'Dynamic Styling',
+            component: Layers
+          },
+          {
+            path: 'controlsExample',
+            name: 'Markers & Popups',
+            component: Controls
+          },
+          {
+            path: 'geojsonExample',
+            name: 'GeoJSON',
+            component: GeoJson
+          },
+          {
+            path: 'geojsonAnimationExample',
+            name: 'GeoJSON Animation',
+            component: GeoJsonAnimation
+          },
+          {
+            path: 'interactionExample',
+            name: 'Limit Map Interaction',
+            component: Interaction
+          },
+          {
+            path: 'viewportAnimationExample',
+            name: 'Camera Transition',
+            component: ViewportAnimation
+          },
+          {
+            path: 'filter',
+            name: 'Highlight By Filter',
+            component: Filter
+          }
+        ]
+      },
+      {
+        name: 'Basic Examples',
+        children: [
+          {
+            path: 'markerExample',
+            name: 'Marker Example',
+            component: MarkerExample
+          },
+          {
+            path: 'notInteractive',
+            name: 'Not Interactive',
+            component: NotInteractiveExample
+          },
+          {
+            path: 'customOverlay',
+            name: 'Custom Overlay',
+            component: CustomOverlayExample
+          },
+          {
+            path: 'routeOverlay',
+            name: 'Route Overlay',
+            component: RouteOverlayExample
+          },
+          {
+            path: 'styleDiffing',
+            name: 'Style Diffing',
+            component: StyleDiffingExample
+          },
+          {
+            path: 'clickExample',
+            name: 'Click',
+            component: ClickExample
+          }
+        ]
+      }
+    ]
+  },
   {
     name: 'Documentation',
     path: '/docs',
@@ -104,9 +192,9 @@ export default [
           {
             name: 'SVGOverlay',
             markdown: svgOverlay
-          },
+          }
         ]
-      },
+      }
     ]
   }
 ];
