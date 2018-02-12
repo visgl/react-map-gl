4bf2694ba5f0f4db4e4dd6837ec83871846304a0
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-02-12 22:32:10 +0000

    refactor: abstract filter and render vessels functions

--- examples/main/views/VesselSVGOverlay.js
+++ examples/main/views/VesselSVGOverlay.js
@@ -2,6 +2,19 @@ import React, {Component} from 'react'
 import {SVGOverlay} from 'react-map-gl' 
 import { arrow } from '../../data/boatPath';
 const helpers = require('../../data/custom-overlay')
+import PropTypes from 'prop-types'
+
+export const defaultRenderVessel = (point, lng, lat, rotation, key = 0) => {
+  return <path
+    key={key}
+    style={ {fill: 'rgba(0,0,0,0)', stroke: '#1FBAD6', fill: 'solid'} }
+    d={arrow}
+    transform={`translate(${point}),rotate(${String(rotation)}),scale(0.02)`}
+    onClick={ () => {
+      const windowAlert = window.alert;
+      windowAlert(`Vessel ${i}`);
+    } }/>
+}
 
 export class VesselSVGOverlay extends Component {
   constructor (props) {
@@ -14,21 +27,15 @@ export class VesselSVGOverlay extends Component {
 
   _redraw (opt) {
     const { locations } = this.state
+    const { filterVessels, renderVessel } = this.props
+
     if (!locations || locations.length === 0) {
       return <g></g>
     }
 
     const vessels = locations.map((location, i) => {
-      const point = opt.project([location.longitude, location.latitude])      
-      return <path
-        key={ i }
-        style={ {fill: 'rgba(0,0,0,0)', stroke: '#1FBAD6', fill: 'solid'} }
-        d={arrow}
-        transform={`translate(${point}),rotate(${String(location.rotation)}),scale(0.02)`}
-        onClick={ () => {
-          const windowAlert = window.alert;
-          windowAlert(`Vessel ${i}`);
-        } }/>
+      const point = opt.project([location.longitude, location.latitude]).filter(filterVessels)
+      return renderVessel(point, location.longitude, location.latitude, location.rotation, i)
     })
 
     return (
@@ -46,6 +53,16 @@ export class VesselSVGOverlay extends Component {
       <SVGOverlay redraw={this._redraw} />
     )
   }
+}
+
+VesselSVGOverlay.PropTypes = {
+  filterVessels: PropTypes.func,
+  renderVessel: PropTypes.func
+}
+
+VesselSVGOverlay.defaultProps = {
+  filterVessels: v => v,
+  renderVessel: (point, lng, lat, rotation, key = 0) => defaultRenderVessel(point, lng, lat, rotation, key)
 }
 
 export default VesselSVGOverlay
