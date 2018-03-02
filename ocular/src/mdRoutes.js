d9c76c53895d3ad00bb84e4a4d4813fd64727a8c
Balthazar Gronon <balthazar@uber.com>
2018-03-01 16:10:11 -0800

    Use default component require

--- ocular/src/mdRoutes.js
+++ ocular/src/mdRoutes.js
@@ -1,95 +1,90 @@
-function makeUrl(url) {
-  return `https://raw.githubusercontent.com/uber-web/probe.gl/master/${url}`;
-}
+export const makeUrl = url =>
+  `https://raw.githubusercontent.com/uber-web/probe.gl/master/${url}`;
 
 export default [
-  // {
-  //   name: 'Examples',
-  //   path: '/examples',
-  //   data: [
-  //     // {
-  //     //   name: 'Overview',
-  //     //   markdown: overview
-  //     // },
-  //     {
-  //       name: 'Showcases',
-  //       children: [
-  //         {
-  //           path: 'layersExample',
-  //           name: 'Dynamic Styling',
-  //           component: require('../../examples/layers/src/app')
+  {
+    name: 'Examples',
+    path: '/examples',
+    data: [
+      {
+        name: 'Showcases',
+        children: [
+          {
+            path: 'layersExample',
+            name: 'Dynamic Styling',
+            component: require('../../examples/layers/src/app').default
 
-  //         },
-  //         {
-  //           path: 'controlsExample',
-  //           name: 'Markers & Popups',
-  //           component: require('../../examples/controls/src/app')
-  //         },
-  //         {
-  //           path: 'geojsonExample',
-  //           name: 'GeoJSON',
-  //           component: require('../../examples/geojson/src/app')
-  //         },
-  //         {
-  //           path: 'geojsonAnimationExample',
-  //           name: 'GeoJSON Animation',
-  //           component: require('../../examples/geojson-animation/src/app')
-  //         },
-  //         {
-  //           path: 'interactionExample',
-  //           name: 'Limit Map Interaction',
-  //           component: require('../../examples/interaction/src/app')
-  //         },
-  //         {
-  //           path: 'viewportAnimationExample',
-  //           name: 'Camera Transition',
-  //           component: require('../../examples/viewport-animation/src/app')
-  //         },
-  //         {
-  //           path: 'filter',
-  //           name: 'Highlight By Filter',
-  //           component: require('../../examples/filter/src/app')
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       name: 'Basic Examples',
-  //       children: [
-  //         {
-  //           path: 'markerExample',
-  //           name: 'Marker Example',
-  //           component: require('../../examples/main/views/marker')
-  //         },
-  //         {
-  //           path: 'notInteractive',
-  //           name: 'Not Interactive',
-  //           component: require('../../examples/main/views/not-interactive')
-  //         },
-  //         {
-  //           path: 'customOverlay',
-  //           name: 'Custom Overlay',
-  //           component: require('../../examples/main/views/custom-overlay')
-  //         },
-  //         {
-  //           path: 'routeOverlay',
-  //           name: 'Route Overlay',
-  //           component: require('../../examples/main/views/route-overlay')
+          },
+          {
+            path: 'controlsExample',
+            name: 'Markers & Popups',
+            component: require('../../examples/controls/src/app').default
+          },
+          {
+            path: 'geojsonExample',
+            name: 'GeoJSON',
+            component: require('../../examples/geojson/src/app').default
+          },
+          {
+            path: 'geojsonAnimationExample',
+            name: 'GeoJSON Animation',
+            component: require('../../examples/geojson-animation/src/app').default
+          },
+          {
+            path: 'interactionExample',
+            name: 'Limit Map Interaction',
+            component: require('../../examples/interaction/src/app').default
+          },
+          {
+            path: 'viewportAnimationExample',
+            name: 'Camera Transition',
+            component: require('../../examples/viewport-animation/src/app').default
+          },
+          {
+            path: 'filter',
+            name: 'Highlight By Filter',
+            component: require('../../examples/filter/src/app').default
+          }
+        ]
+      },
+      {
+        name: 'Basic Examples',
+        children: [
+          {
+            path: 'markerExample',
+            name: 'Marker Example',
+            component: require('../../examples/main/views/marker')
+          },
+          {
+            path: 'notInteractive',
+            name: 'Not Interactive',
+            component: require('../../examples/main/views/not-interactive')
+          },
+          {
+            path: 'customOverlay',
+            name: 'Custom Overlay',
+            component: require('../../examples/main/views/custom-overlay')
+          },
+          {
+            path: 'routeOverlay',
+            name: 'Route Overlay',
+            component: require('../../examples/main/views/route-overlay')
 
-  //         },
-  //         {
-  //           path: 'styleDiffing',
-  //           name: 'Style Diffing',
-  //           component: require('../../examples/main/views/style-diffing')
-  //         },
-  //         {
-  //           path: 'clickExample',
-  //           name: 'Click',
-  //           component: require('../../examples/main/views/click')
-  //         }
-  //       ]
-  //     }
-  //   ]
-  // },
+          },
+          {
+            path: 'styleDiffing',
+            name: 'Style Diffing',
+            component: require('../../examples/main/views/style-diffing')
+          },
+          {
+            path: 'clickExample',
+            name: 'Click',
+            component: require('../../examples/main/views/click')
+          }
+        ]
+      }
+    ]
+  },
   {
     name: 'Documentation',
     path: '/docs',
