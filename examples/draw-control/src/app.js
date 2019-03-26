43a28c7645c36dff52eda4f0dc01512f1f5eab1d
Xintong Xia <xintong@uber.com>
2019-03-26 14:32:13 -0700

    remove debugging dataset

--- examples/draw-control/src/app.js
+++ examples/draw-control/src/app.js
@@ -3,8 +3,6 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import MapGL, {DrawControl} from 'react-map-gl';
 
-import data from './sample.geo';
-
 const MODES = [
   {name: 'Read Only', value: DrawControl.READ_ONLY},
   {name: 'Select Feature', value: DrawControl.SELECT_FEATURE},
@@ -13,73 +11,6 @@ const MODES = [
   {name: 'Draw Path', value: DrawControl.DRAW_PATH},
   {name: 'Draw Polygon', value: DrawControl.DRAW_POLYGON}
 ];
-
-function flatFeatures(geoJson, parentId = '0') {
-  if (!geoJson) {
-    return null;
-  }
-
-  let features = [];
-  const {type} = geoJson;
-
-  if (type === 'FeatureCollection') {
-    geoJson.features.forEach((feature, i) => {
-      features = features.concat(flatFeatures(feature, `${parentId}.${i}`));
-    });
-  } else if (type === 'Feature') {
-    const featureType = geoJson.geometry.type;
-
-    switch (featureType) {
-    case 'Point':
-    case 'Polygon':
-    case 'LineString':
-      geoJson.properties.id = `${parentId}.0`;
-      features.push(geoJson);
-      break;
-
-    case 'MultiPoint':
-    case 'MultiPolygon':
-    case 'MultiLineString':
-      geoJson.geometry.coordinates.forEach((coordinates, i) => {
-        features.push({
-          type: 'Feature',
-          geometry: {
-            type: (/Multi(\w+)/).exec(featureType)[1],
-            coordinates
-          },
-          properties: {
-            ...geoJson.properties,
-            id: `${parentId}.${i}`
-          }
-        });
-      });
-      break;
-
-    case 'GeometryCollection':
-      geoJson.geometry.geometries.forEach((g, i) => {
-        const id = `${parentId}.${i}`;
-        const singleFeature = {
-          type: 'Feature',
-          geometry: {
-            type: g.type,
-            coordinates: g.coordinates
-          },
-          properties: {
-            ...geoJson.properties,
-            id
-          }
-        };
-
-        features.concat(flatFeatures(singleFeature, id));
-      });
-      break;
-
-    default:
-    }
-  }
-
-  return features;
-}
 
 export default class App extends Component {
 
@@ -94,7 +25,7 @@ export default class App extends Component {
         zoom: 14
       },
       mode: DrawControl.READ_ONLY,
-      features: flatFeatures(data),
+      features: [],
       selectedId: null
     };
   }
