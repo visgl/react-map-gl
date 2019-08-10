cd46e5480400ad6ce651f653ddffb09be0648cb2
Hossein Moradi Davijani <moradidavijani@gmail.com>
2019-08-10 12:38:25 +0430

    Fix deprecated React lifecycles in examples

--- examples/main/views/style-diffing.js
+++ examples/main/views/style-diffing.js
@@ -80,7 +80,7 @@ export default class StyleDiffingExample extends Component {
     this._onClick = this._onClick.bind(this);
   }
 
-  componentWillMount() {
+  UNSAFE_componentWillMount() {
     const colors = ['red', 'green', 'blue'];
     let i = 0;
     this._intervalId = window.setInterval(
