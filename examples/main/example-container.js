0b2cecbb1d51f09559f7dd8638b37abbbf70c967
Hossein Moradi Davijani <moradidavijani@gmail.com>
2019-08-12 22:31:40 +0430

    React 16.9.0 deprecated lifecycles (#861)

--- examples/main/example-container.js
+++ examples/main/example-container.js
@@ -32,7 +32,7 @@ export default class App extends Component {
     };
   }
 
-  componentWillMount() {
+  UNSAFE_componentWillMount() {
     window.onresize = () =>
       this.setState({
         viewport: {
