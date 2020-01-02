58649151254a5215744723035a72117721e31b4a
Xiaoji Chen <xiaoji@uber.com>
2020-01-01 20:23:17 -0800

    lint

--- examples/clusters/src/layers.js
+++ examples/clusters/src/layers.js
@@ -4,27 +4,11 @@ export const clusterLayer = {
   source: 'earthquakes',
   filter: ['has', 'point_count'],
   paint: {
-    'circle-color': [
-      'step',
-      ['get', 'point_count'],
-      '#51bbd6',
-      100,
-      '#f1f075',
-      750,
-      '#f28cb1'
-    ],
-    'circle-radius': [
-      'step',
-      ['get', 'point_count'],
-      20,
-      100,
-      30,
-      750,
-      40
-    ]
+    'circle-color': ['step', ['get', 'point_count'], '#51bbd6', 100, '#f1f075', 750, '#f28cb1'],
+    'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40]
   }
 };
- 
+
 export const clusterCountLayer = {
   id: 'cluster-count',
   type: 'symbol',
@@ -36,7 +20,7 @@ export const clusterCountLayer = {
     'text-size': 12
   }
 };
- 
+
 export const unclusteredPointLayer = {
   id: 'unclustered-point',
   type: 'circle',
