46efaa9dd830aa0940c29a5d70d8c479240f732b
Harley Alexander <GISmac@CoMs-MacBook-Pro.local>
2020-05-21 17:03:44 +1000

    🏷 add flow type to useMapRef hook return value

--- src/components/use-map-ref.js
+++ src/components/use-map-ref.js
@@ -1,11 +1,14 @@
-import MapContext from './map-context';
+// @flow
+
 import {useContext} from 'react';
+import Mapbox from '../mapbox/mapbox';
+import MapContext from './map-context';
 
 /**
  * Hook to access underlying Mapbox instance. Use only available
  * inside a child component of InteractiveMap or StaticMap.
  */
-export const useMapRef = () => {
+export const useMapRef = (): Mapbox | null => {
   const context = useContext(MapContext);
   if (!context) throw Error('Trying to useMapRef outside of MapContext.');
   return context.map;
