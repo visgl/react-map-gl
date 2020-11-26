92e0a556b1921dd29e5cec0b3a377227b4463033
Xiaoji Chen <cxiaoji@gmail.com>
2020-11-26 14:03:48 -0800

    update Layer and Source to functional components

--- src/components/source.js
+++ src/components/source.js
@@ -19,13 +19,12 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 import * as React from 'react';
-import {PureComponent, cloneElement} from 'react';
+import {useContext, useEffect, useMemo, useState, useRef} from 'react';
+import {cloneElement} from 'react';
 import PropTypes from 'prop-types';
 import MapContext from './map-context';
 import assert from '../utils/assert';
 import deepEqual from '../utils/deep-equal';
-
-import type {MapContextProps} from './map-context';
 
 const propTypes = {
   type: PropTypes.string.isRequired,
@@ -35,131 +34,125 @@ const propTypes = {
 type SourceProps = {
   id?: string,
   type: string,
-  children?: any
+  children?: any,
+  data?: any,
+  coordinates?: any,
+  url?: any,
+  tiles?: any
 };
 
 let sourceCounter = 0;
 
-export default class Source<Props: SourceProps> extends PureComponent<Props> {
-  static propTypes = propTypes;
-
-  constructor(props: Props) {
-    super(props);
-    this.id = props.id || `jsx-source-${sourceCounter++}`;
-    this.type = props.type;
-  }
-
-  componentWillUnmount() {
-    /* global requestAnimationFrame */
-    // Do not remove source immediately because the
-    // dependent <Layer>s' componentWillUnmount() might not have been called
-    // Removing source before dependent layers will throw error
-    // TODO - find a more robust solution
-    const map = this._map;
-    if (map) {
-      map.off('styledata', this._updateSource);
-      requestAnimationFrame(() => {
-        if (map.style && map.style._loaded) {
-          map.removeSource(this.id);
-        }
-      });
-    }
-  }
-
-  id: string;
-  type: string;
-  _map: any = null;
-  _sourceOptions: any = {};
-
-  getSource() {
-    const map = this._map;
-    return map && map.style && map.getSource(this.id);
-  }
-
-  _createSource(sourceOptions: any) {
-    const map = this._map;
-    if (map.style && map.style._loaded) {
-      map.addSource(this.id, sourceOptions);
-    }
-  }
-
-  /* eslint-disable complexity */
-  _updateSource = () => {
-    const {type, _map: map} = this;
-    if (!map) {
-      return;
-    }
-
-    const {_sourceOptions: sourceOptions, props} = this;
-    assert(!props.id || props.id === this.id, 'source id changed');
-    assert(props.type === type, 'source type changed');
-
-    let changedKey = '';
-    let changedKeyCount = 0;
-
-    for (const key in props) {
-      if (key !== 'children' && key !== 'id' && !deepEqual(sourceOptions[key], props[key])) {
-        sourceOptions[key] = props[key];
-        changedKey = key;
-        changedKeyCount++;
-      }
-    }
-
-    const source = this.getSource();
-    if (!source) {
-      this._createSource(sourceOptions);
-      return;
-    }
-    if (!changedKeyCount) {
-      return;
-    }
-    if (type === 'geojson') {
-      source.setData(sourceOptions.data);
-    } else if (type === 'image') {
-      source.updateImage({url: sourceOptions.url, coordinates: sourceOptions.coordinates});
-    } else if (
-      (type === 'canvas' || type === 'video') &&
-      changedKeyCount === 1 &&
-      changedKey === 'coordinates'
-    ) {
-      source.setCoordinates(sourceOptions.coordinates);
-    } else if (type === 'vector' && source.setUrl) {
-      // Added in 1.12.0:
-      // vectorTileSource.setTiles
-      // vectorTileSource.setUrl
-      switch (changedKey) {
-        case 'url':
-          source.setUrl(sourceOptions.url);
-          break;
-        case 'tiles':
-          source.setTiles(sourceOptions.tiles);
-          break;
-        default:
-      }
-    } else {
-      // eslint-disable-next-line
-      console.warn(`Unable to update <Source> prop: ${changedKey}`);
-    }
-  };
-  /* eslint-enable complexity */
-
-  _render(context: MapContextProps) {
-    if (!this._map && context.map) {
-      this._map = context.map;
-      this._map.on('styledata', this._updateSource);
-    }
-    this._updateSource();
-    return React.Children.map(
-      this.props.children,
-      child =>
-        child &&
-        cloneElement(child, {
-          source: this.id
-        })
-    );
-  }
-
-  render() {
-    return <MapContext.Consumer>{this._render.bind(this)}</MapContext.Consumer>;
+function createSource(map, id, props) {
+  if (map.style && map.style._loaded) {
+    const options = {...props};
+    delete options.id;
+    delete options.children;
+    map.addSource(id, options);
   }
 }
+
+/* eslint-disable complexity */
+function updateSource(source, props, prevProps) {
+  assert(props.id === prevProps.id, 'source id changed');
+  assert(props.type === prevProps.type, 'source type changed');
+
+  let changedKey = '';
+  let changedKeyCount = 0;
+
+  for (const key in props) {
+    if (key !== 'children' && key !== 'id' && !deepEqual(prevProps[key], props[key])) {
+      changedKey = key;
+      changedKeyCount++;
+    }
+  }
+
+  if (!changedKeyCount) {
+    return;
+  }
+
+  const {type} = props;
+  if (type === 'geojson') {
+    source.setData(props.data);
+  } else if (type === 'image') {
+    source.updateImage({url: props.url, coordinates: props.coordinates});
+  } else if (
+    (type === 'canvas' || type === 'video') &&
+    changedKeyCount === 1 &&
+    changedKey === 'coordinates'
+  ) {
+    source.setCoordinates(props.coordinates);
+  } else if (type === 'vector' && source.setUrl) {
+    // Added in 1.12.0:
+    // vectorTileSource.setTiles
+    // vectorTileSource.setUrl
+    switch (changedKey) {
+      case 'url':
+        source.setUrl(props.url);
+        break;
+      case 'tiles':
+        source.setTiles(props.tiles);
+        break;
+      default:
+    }
+  } else {
+    // eslint-disable-next-line
+    console.warn(`Unable to update <Source> prop: ${changedKey}`);
+  }
+}
+/* eslint-enable complexity */
+
+function Source(props: SourceProps) {
+  const context = useContext(MapContext);
+  const propsRef = useRef<SourceProps>({id: props.id, type: props.type});
+  const [, setStyleLoaded] = useState(0);
+
+  const id = useMemo(() => props.id || `jsx-source-${sourceCounter++}`, []);
+  const {map} = context;
+
+  useEffect(
+    () => {
+      if (map) {
+        const forceUpdate = () => setStyleLoaded(version => version + 1);
+        map.on('styledata', forceUpdate);
+
+        return () => {
+          map.off('styledata', forceUpdate);
+          /* global requestAnimationFrame */
+          // Do not remove source immediately because the
+          // dependent <Layer>s' componentWillUnmount() might not have been called
+          // Removing source before dependent layers will throw error
+          // TODO - find a more robust solution
+          requestAnimationFrame(() => {
+            if (map.style && map.style._loaded) {
+              map.removeSource(id);
+            }
+          });
+        };
+      }
+      return undefined;
+    },
+    [map]
+  );
+
+  const source = map && map.style && map.getSource(id);
+  if (source) {
+    updateSource(source, props, propsRef.current);
+  } else {
+    createSource(map, id, props);
+  }
+
+  return source
+    ? React.Children.map(
+        props.children,
+        child =>
+          child &&
+          cloneElement(child, {
+            source: id
+          })
+      )
+    : null;
+}
+
+Source.propTypes = propTypes;
+export default Source;
