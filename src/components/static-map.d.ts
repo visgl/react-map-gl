24ec2ff510142c40a07f76716d0e2d84d5bc0cef
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2020-12-21 09:52:48 -0800

    Migrate to TypeScript: generate flow types (#1269)

--- src/components/static-map.d.ts
+++ src/components/static-map.d.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import type {ViewState, MapboxProps} from '../mapbox/mapbox';
+import type {MapboxProps} from '../mapbox/mapbox';
 
 export type StaticMapProps = MapboxProps & Partial<{
   className: string,
