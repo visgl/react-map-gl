20ad2c10a208638affeb9b23217a438eece91e73
Xiaoji Chen <xiac@microsoft.com>
2021-12-30 13:47:30 -0800

    try fix CI

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,22 @@ const {getWebpackConfig} = require('ocular-dev-tools');
 module.exports = env => {
   const config = getWebpackConfig(env);
 
+  config.resolve = {...config.resolve, extensions: ['.ts', '.tsx', '.js', '.json']};
+
+  config.module.rules = [
+    ...config.module.rules.filter(r => r.loader !== 'babel-loader'),
+    {
+      // Compile source using babel
+      test: /(\.js|\.ts|\.tsx)$/,
+      loader: 'babel-loader',
+      exclude: [/node_modules/],
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+        plugins: ['@babel/plugin-proposal-class-properties']
+      }
+    }
+  ];
+
   config.plugins = (config.plugins || []).concat([
     new webpack.DefinePlugin({
       __MAPBOX_TOKEN__: JSON.stringify(process.env.MapboxAccessToken) // eslint-disable-line
