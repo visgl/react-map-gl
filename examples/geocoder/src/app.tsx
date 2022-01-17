aa910e2c67b370f80decad181debf34858ab4ce6
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2022-01-16 23:59:23 -0800

    Add geocoder example (#1690)

---
+++ examples/geocoder/src/app.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import {render} from 'react-dom';
+import Map from 'react-map-gl';
+
+import GeocoderControl from './geocoder-control';
+import ControlPanel from './control-panel';
+
+const TOKEN = process.env.MapboxAccessToken; // Set your mapbox token here
+
+export default function App() {
+  return (
+    <>
+      <Map
+        initialViewState={{
+          longitude: -79.4512,
+          latitude: 43.6568,
+          zoom: 13
+        }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={TOKEN}
+      >
+        <GeocoderControl mapboxAccessToken={TOKEN} position="top-left" />
+        <ControlPanel />
+      </Map>
+    </>
+  );
+}
+
+export function renderToDom(container) {
+  render(<App />, container);
+}
