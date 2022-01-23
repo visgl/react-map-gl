0bc8697ecb7a237a7338a45b6c783bcde3f2264f
Xiaoji Chen <cxiaoji@gmail.com>
2022-01-22 17:01:11 -0800

    tweak

--- src/utils/apply-react-style.ts
+++ src/utils/apply-react-style.ts
@@ -1,18 +1,20 @@
 import * as React from 'react';
-// https://github.com/facebook/react/blob/4131af3e4bf52f3a003537ec95a1655147c81270/src/renderers/dom/shared/CSSProperty.js
-const unitlessNumber = /^(box|flex|grid|column|lineHeight|fontWeight|opacity|order|tabSize|zIndex)/;
+// This is a simplified version of
+// https://github.com/facebook/react/blob/4131af3e4bf52f3a003537ec95a1655147c81270/src/renderers/dom/shared/CSSPropertyOperations.js#L62
+const unitlessNumber = /box|flex|grid|column|lineHeight|fontWeight|opacity|order|tabSize|zIndex/;
 
-export function applyReactStyle(element: HTMLElement, style: React.CSSProperties) {
-  if (!element || !style) {
+export function applyReactStyle(element: HTMLElement, styles: React.CSSProperties) {
+  if (!element || !styles) {
     return;
   }
+  const style = element.style;
 
-  for (const key in style) {
-    const value = style[key];
+  for (const key in styles) {
+    const value = styles[key];
     if (Number.isFinite(value) && !unitlessNumber.test(key)) {
-      element.style[key] = `${value}px`;
+      style[key] = `${value}px`;
     } else {
-      element.style[key] = value;
+      style[key] = value;
     }
   }
 }
