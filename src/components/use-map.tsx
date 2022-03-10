6244d55dd06b0fb6289ed6a5bad4f2f5c97a42d3
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2022-03-10 08:11:48 -0800

    Fix useMap type (#1799)

--- src/components/use-map.tsx
+++ src/components/use-map.tsx
@@ -51,7 +51,7 @@ export const MapProvider: React.FC<{}> = props => {
   );
 };
 
-export function useMap(): {current?: MapRef; [id: string]: MapRef} {
+export function useMap(): {current?: MapRef; [id: string]: MapRef | undefined} {
   const maps = useContext(MountedMapsContext)?.maps;
   const currentMap = useContext(MapContext);
 
