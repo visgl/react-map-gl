0b2f7f525e5c43981d62cb02316a8cc3f9ab88f7
Xiaoji Chen <cxiaoji@gmail.com>
2022-06-10 21:19:59 -0700

    Update deckgl-overlay example

--- examples/deckgl-overlay/src/app.tsx
+++ examples/deckgl-overlay/src/app.tsx
@@ -1,34 +1,51 @@
 import * as React from 'react';
 import {createRoot} from 'react-dom/client';
-import DeckGL, {ArcLayer} from 'deck.gl';
-import Map from 'react-map-gl';
+import {ScatterplotLayer} from '@deck.gl/layers/typed';
+import {DeckProps} from '@deck.gl/core/typed';
+import {MapboxOverlay} from '@deck.gl/mapbox/typed';
+import {useControl} from 'react-map-gl';
+
+import Map, {NavigationControl} from 'react-map-gl';
 
 const TOKEN = ''; // Set your mapbox token here
 
+const initialViewState = {
+  latitude: 37.78,
+  longitude: -122.45,
+  zoom: 12
+};
+
+function DeckGLOverlay(props: DeckProps) {
+  const deck = useControl<MapboxOverlay>(() => new MapboxOverlay(props));
+
+  deck.setProps(props);
+  return null;
+}
+
 export default function App() {
-  const arcLayer = new ArcLayer({
-    data: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/bart-segments.json',
-    getSourcePosition: d => d.from.coordinates,
-    getTargetPosition: d => d.to.coordinates,
-    getSourceColor: [255, 200, 0],
-    getTargetColor: [0, 140, 255],
-    getWidth: 12
+  const scatterplotLayer = new ScatterplotLayer<{
+    name: string;
+    entries: number;
+    exists: number;
+    coordinates: [number, number];
+  }>({
+    data: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/bart-stations.json',
+    getPosition: d => d.coordinates,
+    getRadius: d => Math.sqrt(d.entries),
+    getFillColor: [255, 0, 0],
+    pickable: true,
+    autoHighlight: true
   });
 
   return (
-    <DeckGL
-      initialViewState={{
-        longitude: -122.45,
-        latitude: 37.75,
-        zoom: 11,
-        bearing: 0,
-        pitch: 60
-      }}
-      controller={true}
-      layers={[arcLayer]}
+    <Map
+      initialViewState={initialViewState}
+      mapStyle="mapbox://styles/mapbox/light-v9"
+      mapboxAccessToken={TOKEN}
     >
-      <Map mapStyle="mapbox://styles/mapbox/light-v9" mapboxAccessToken={TOKEN} />
-    </DeckGL>
+      <DeckGLOverlay layers={[scatterplotLayer]} />
+      <NavigationControl />
+    </Map>
   );
 }
 
