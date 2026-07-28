2a22d6a3427b9175830d810f0041613511944f38
Chris Gervang <chris.gervang@joby.aero>
2026-07-28 16:55:24 -0700

    ci: test MapLibre v6

--- test/browser.js
+++ test/browser.js
@@ -1,10 +1,12 @@
 /* global window */
 import test from 'tape';
-import {setWorkerUrl} from 'maplibre-gl';
+import {getVersion, setWorkerUrl} from 'maplibre-gl';
 
 // MapLibre v6 resolves its worker relative to import.meta.url. Vite prebundles
 // dependencies into .vite/deps, where the sibling worker file is not present.
-setWorkerUrl('/node_modules/maplibre-gl/dist/maplibre-gl-worker.mjs');
+if (Number.parseInt(getVersion(), 10) >= 6) {
+  setWorkerUrl('/node_modules/maplibre-gl/dist/maplibre-gl-worker.mjs');
+}
 
 test.onFinish(window.browserTestDriver_finish);
 test.onFailure(window.browserTestDriver_fail);
