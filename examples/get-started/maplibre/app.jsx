798683432cdafd6f154599005f3d90f0d81effcf
Chris Gervang <chrisgervang@users.noreply.github.com>
2026-07-29 09:03:24 -0700

    chore: upgrade website and examples to MapLibre v6 (#2602)

--- examples/get-started/maplibre/app.jsx
+++ examples/get-started/maplibre/app.jsx
@@ -1,9 +1,14 @@
 /* global document */
 import * as React from 'react';
 import {createRoot} from 'react-dom/client';
+// eslint-disable-next-line import/namespace
+import {setWorkerUrl} from 'maplibre-gl';
+import workerUrl from 'maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url';
 import Map, {Marker} from 'react-map-gl/maplibre';
 
 import 'maplibre-gl/dist/maplibre-gl.css';
+
+setWorkerUrl(workerUrl);
 
 function Root() {
   return (
